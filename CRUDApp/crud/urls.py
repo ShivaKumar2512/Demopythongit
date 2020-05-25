bd5ed84993e3254d2ee5621a79458624ac85b2b1
Shiva Kumar Gone <goneshivakumar@gmail.com>
2020-05-25 16:49:19 +0530

    Records Display Order Creation

--- CRUDApp/crud/urls.py
+++ CRUDApp/crud/urls.py
@@ -2,10 +2,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import home, products, customers
+from .views import home, products, customers, create_order
 
 urlpatterns = [
     path('', home, name='home'),
     path('products', products, name='products'),
-    path('customers', customers, name='customers'),
+    path('customers/<str:pk_test>/', customers, name='customers'),
+    path('create_order', create_order, name='create_order')
 ]
--- CRUDApp/crud/views.py
+++ CRUDApp/crud/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import OrderForm
 from .models import *
 
 
@@ -7,14 +9,48 @@ from .models import *
 
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_orders =orders.count()
+    delivered = orders.filter(status = 'Delivered').count()
+    pending = orders.filter(status = 'Pending').count()
+
+    context = {'orders': orders, 'customers': customers,
+               'total_orders':total_orders,
+               'delivered':delivered,
+               'pending':pending}
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def products(request):
     products = Product.objects.all()
-    return render(request, 'accounts/products.html',{'products':products})
+    return render(request, 'accounts/products.html', {'products': products})
 
 
-def customers(request):
-    customers = Customer.objects.all()
-    return render(request, 'accounts/customers.html',{'customers':customers})
+def customers(request, pk_test):
+    print(pk_test)
+    customer = Customer.objects.get(id = pk_test)
+    orders = customer.order_set.all()
+    context = {
+        'customer':customer,
+        'orders':orders,
+        'orders_count':orders.count()
+    }
+    #print(customer)
+    return render(request, "accounts/customers.html",context)
+
+
+def create_order(request):
+    form = OrderForm()
+    if request.method == 'POST':
+        # print('Printing POST Values',request.POST)
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {
+    'form':form
+    }
+    return render(request,"accounts/order.html",context)
